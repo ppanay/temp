16634f2409a0a406fdd7098d5a717f231824ebd1
ppanay <ppanayotes@gmx.com>
2020-10-18 14:48:58 +0300

    twelve

--- one.c
+++ one.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+#include <complex.h>
 
 int main() {
-    int i,a,b,c;
+    int i,a,b,c,robincat=7;
     a=b=c=i=1;
     for(i=0;i<1000;i++) {
         a--;
         b++;
-        for(int j=0;j<1E7;j++) c=3*a+b+i+c;
+        for(int j=0;j<2E9;j++) c=3*a+b+i+c;
         printf("%d\n",c);
     }
     printf("c= %d\n",c);
     printf("Ended!\n");
     printf("Yoooooooooooooooo");
 }
+
+
+
--- one.py
+++ one.py
@@ -4,3 +4,11 @@ c=4
 c=c+1231415
 print(c)
 print("xaxaxa!")
+robincat=27
+robincat+=1
+robincat = robincat + 3
+c=c+3
+d=range(80)
+import time
+time.sleep(5)
+print('Hiiiiiiii')
